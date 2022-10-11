5bc48657115921a7b78f7ffdcc73dd4da32009fd
Blake Larson <Blarson224@gmail.com>
2022-10-11 10:54:24 -0700

    testing cors

--- server/server.js
+++ server/server.js
@@ -23,14 +23,14 @@ app.use(
 		origin: 'https://mymusicstudio.netlify.app',
 	})
 );
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
+// app.use(function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	next();
+// });
 
 // Passport config
 require('./config/passport')(passport);
