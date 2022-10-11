97a4c01371a16b3de649c43c24d8387b840eb3d3
Blake Larson <Blarson224@gmail.com>
2022-10-11 09:57:42 -0700

    testing cors

--- server/server.js
+++ server/server.js
@@ -15,24 +15,23 @@ const cors = require('cors');
 
 require('dotenv').config({ path: './config/.env' });
 
-// app.use(
-// 	cors({
-// 		credentials: true,
-// 		origin: 'https://mymusicstudio.netlify.app/',
-// 	})
-// );
+//app.use(cors());
 
-app.use(cors());
-
+app.use(
+	cors({
+		credentials: true,
+		origin: 'https://mymusicstudio.netlify.app/',
+	})
+);
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	next();
+// });
 
 // Passport config
 require('./config/passport')(passport);
