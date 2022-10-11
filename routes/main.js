020db84527402973d44d90a4860b45809eb9d3a9
Blake Larson <Blarson224@gmail.com>
2022-10-11 08:55:34 -0700

    fixing heroku

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
 
-router.get('/', authController.serverMessage);
+//router.get('/', authController.serverMessage);
 router.get('/logout', authController.logout);
 router.get('/authenticated', authController.getAuthenticated);
 
