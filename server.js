a93ff04d81afe01447ddd214e605cd066bed3c0c
Blake Larson <Blarson224@gmail.com>
2022-10-11 12:56:24 -0700

    trying heroku

--- server.js
+++ server.js
@@ -42,12 +42,9 @@ app.use(
 	})
 );
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('build'));
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.join('clinet', 'build', 'index.html'));
-// 	});
-// }
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 // Passport middleware
 app.use(passport.initialize());
