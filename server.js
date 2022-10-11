7a0d6dd183891d6a38aaf8e5f1c51b7deb7d3fee
Blake Larson <Blarson224@gmail.com>
2022-10-11 14:01:18 -0700

    trying heroku

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const path = require('path');
 console.log(path.resolve(__dirname, 'client/build', 'index.html'));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('build'));
+	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 	});
@@ -55,10 +55,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use('/', mainRoutes);
-// app.use('/students', studentRoutes);
-// app.use('/lessons', lessonRoutes);
-// app.use('/todos', todoRoutes);
+app.use('/', mainRoutes);
+app.use('/students', studentRoutes);
+app.use('/lessons', lessonRoutes);
+app.use('/todos', todoRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log('Server is running, you better catch it!');
