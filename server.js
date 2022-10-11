13c809160cd6c75c34cd340ee29860c2ed264c15
Blake Larson <Blarson224@gmail.com>
2022-10-11 08:23:20 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ const cors = require('cors');
 
 require('dotenv').config({ path: './config/.env' });
 
-app.use(
-	cors({
-		credentials: true,
-		origin: 'https://mymusicstudio.netlify.app/',
-	})
-);
+// app.use(
+// 	cors({
+// 		credentials: true,
+// 		origin: 'https://mymusicstudio.netlify.app/',
+// 	})
+// );
+
+app.use(cors());
 
 // Passport config
 require('./config/passport')(passport);
@@ -50,6 +52,16 @@ app.use('/', mainRoutes);
 app.use('/students', studentRoutes);
 app.use('/lessons', lessonRoutes);
 app.use('/todos', todoRoutes);
+
+// Accessing the path module
+const path = require('path');
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, './client/build')));
+// Step 2:
+app.get('*', function (request, response) {
+	response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT, () => {
 	console.log('Server is running, you better catch it!');
