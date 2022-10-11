8e59af77b606f245c020e5c0034b1cf4b47a9d07
Blake Larson <Blarson224@gmail.com>
2022-10-11 13:30:09 -0700

    trying heroku

--- server.js
+++ server.js
@@ -44,8 +44,9 @@ app.use(
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, './client/public', 'index.html'));
+		res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 	});
 }
 
