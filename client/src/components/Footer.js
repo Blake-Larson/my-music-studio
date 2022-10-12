2e1d87d2319620bfde01dc4b0fbcdca3c2c7c68c
Blake Larson <Blarson224@gmail.com>
2022-10-12 16:49:28 -0700

    added portfolio button

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,13 @@ function Footer() {
 		<footer className='bg-neutral p-4 text-neutral-content mt-auto w-full'>
 			<div className='flex items-center md:justify-between flex-col md:flex-row gap-3'>
 				<LogoIcon />
+
+				<a
+					href='https://blakelarson.dev/'
+					className='text-center btn btn-outline normal-case btn-primary btn-sm'
+				>
+					More work by Blake Larson
+				</a>
 				<p>Copyright © 2022 - All right reserved</p>
 			</div>
 		</footer>
