8a271b421677b5ce93048474c933d8668d0cda8b
Blake Larson <Blarson224@gmail.com>
2022-10-27 08:06:09 -0700

    fixing main bg color

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,18 +3,19 @@ import LogoIcon from './LogoIcon';
 
 function Footer() {
 	return (
-		<footer className='bg-neutral p-4 mt-auto w-full text-base-100 custom-10vh'>
-			<div className='flex items-center md:justify-between flex-col md:flex-row gap-3'>
-				<LogoIcon />
-
+		<footer className='bg-neutral p-4 mt-auto w-full text-base-100 custom-10vh grid md:grid-cols-3 items-center grid-cols-1'>
+			<LogoIcon />
+			<div>
 				<a
 					href='https://blakelarson.dev/'
 					className='text-center btn btn-outline normal-case btn-primary btn-sm'
 				>
 					More work by Blake Larson
 				</a>
-				<p>Copyright © 2022 - All right reserved</p>
 			</div>
+			<p className='md:text-end text-center'>
+				Copyright © 2022 - All right reserved
+			</p>
 		</footer>
 	);
 }
