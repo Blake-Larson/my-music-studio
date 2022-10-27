a121c5029988ecc727f6573ccfe2545f34580702
Blake Larson <Blarson224@gmail.com>
2022-10-27 08:20:11 -0700

    fixed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 	return (
 		<footer className='bg-neutral p-4 mt-auto w-full text-base-100 custom-10vh grid md:grid-cols-3 items-center grid-cols-1'>
 			<LogoIcon />
-			<div>
+			<div className='text-center'>
 				<a
 					href='https://blakelarson.dev/'
 					className='text-center btn btn-outline normal-case btn-primary btn-sm'
