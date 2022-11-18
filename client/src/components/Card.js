c86a4b649361950e73d419f66004d5bac50607e0
Blake Larson <Blarson224@gmail.com>
2022-11-17 16:13:44 -0800

    fixed card height

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,7 +6,7 @@ import manage from '../assets/images/manage.jpg';
 function Card() {
 	return (
 		<div className='flex md:justify-evenly md:flex-row flex-col gap-5 items-center'>
-			<div className='card md:w-96 bg-base-100 shadow-xl'>
+			<div className='card md:w-96 bg-base-100 shadow-xl md:custom-card-height'>
 				<figure>
 					<img src={organize} alt='keyboard, paper, and writing utensils' />
 				</figure>
@@ -18,7 +18,7 @@ function Card() {
 					</p>
 				</div>
 			</div>
-			<div className='card md:w-96 bg-base-100 shadow-xl'>
+			<div className='card md:w-96 bg-base-100 shadow-xl md:custom-card-height'>
 				<figure>
 					<img src={teach} alt='Young person being taught guitar' />
 				</figure>
@@ -27,7 +27,7 @@ function Card() {
 					<p>Save time so you can spend more time teaching your students!</p>
 				</div>
 			</div>
-			<div className='card md:w-96 bg-base-100 shadow-xl'>
+			<div className='card md:w-96 bg-base-100 shadow-xl md:custom-card-height'>
 				<figure>
 					<img src={manage} alt='Woman and child learning piano' />
 				</figure>
